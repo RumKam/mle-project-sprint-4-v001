df2aa3876c5c66c4f6a03677cc920cf39239d6c6
Rumiia Sinelnikova <112545996+RumKam@users.noreply.github.com>
2025-02-10 21:54:09 +0300

    Create config.py

---
+++ recsys/config.py
@@ -0,0 +1,8 @@
+config = {
+    "RECS_URL": "http://127.0.0.1:8000",
+    "FEATURE_STORE_URL": "http://127.0.0.1:8010",
+    "EVENTS_STORE_URL": "http://127.0.0.1:8020",
+    "DEFAULT_PATH": "recommendations/test_top_popular.parquet",
+    "PERSONAL_RECS_PATH": "recommendations/test_recommendations.parquet",
+    "SIMILAR_RECS_PATH": "recommendations/similar_items.parquet"
+     }
